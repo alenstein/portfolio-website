51fe57e1dd1368f4c48bdd4f2b1a755b0df176e1
Allen Sibanda <sibandallen@gmail.com>
2024-10-22 18:30:26 +0200

    clean: prep for v2 portfolio sync

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+//
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('theme-toggle');
+  const body = document.body;
+  const currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme === 'dark') {
+      body.classList.add('dark-mode');
+      toggleBtn.textContent = '☀️';
+  }
+
+  toggleBtn.addEventListener('click', () => {
+      body.classList.toggle('dark-mode');
+
+      let theme = 'light';
+      if (body.classList.contains('dark-mode')) {
+          theme = 'dark';
+          toggleBtn.textContent = '☀️';
+      } else {
+          toggleBtn.textContent = '🌙';
+      }
+
+      localStorage.setItem('theme', theme);
+  });
+});
+
+
